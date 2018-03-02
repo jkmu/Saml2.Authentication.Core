45da4f89777c97a57119ac639a21a2289162de9f
Musyimijnr <kyalo.jessy@gmail.com>
2018-03-02 13:52:44 +0100

    init solution and Saml2.Authentication.Core project with a dummy authentication handler

---
+++ Source/Saml2.Authentication.Core/Authentication/SamlAuthenticationDefaults.cs
@@ -0,0 +1,11 @@
+﻿namespace Saml2.Authentication.Core.Authentication
+{
+    public class SamlAuthenticationDefaults
+    {
+        public const string AuthenticationScheme = "saml2.authentication";
+
+        public const string AuthenticationSchemeDisplayName = "saml2.authentication";
+
+        public const string SignInScheme = "saml2.authentication.cookies";
+    }
+}
---
+++ Source/Saml2.Authentication.Core/Authentication/SamlAuthenticationHandler.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Saml2.Authentication.Core.Authentication
+{
+    public class SamlAuthenticationHandler : AuthenticationHandler<SamlAuthenticationOptions>, IAuthenticationRequestHandler
+    {
+        public SamlAuthenticationHandler(
+            IOptionsMonitor<SamlAuthenticationOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
+
+        public async Task<bool> HandleRequestAsync()
+        {
+            if (await HandleSignIn())
+            {
+                return true;
+            }
+
+            return await HandleSignout();
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            return Task.FromResult(AuthenticateResult.Fail("Not supported"));
+        }
+
+
+        protected override Task HandleChallengeAsync(AuthenticationProperties properties)
+        {
+            Response.Redirect(Options.AssertionConsumerServiceUrl);
+            return Task.FromResult(0);
+        }
+
+        protected override Task InitializeHandlerAsync()
+        {
+            return base.InitializeHandlerAsync();
+        }
+
+        protected override Task HandleForbiddenAsync(AuthenticationProperties properties)
+        {
+            return base.HandleForbiddenAsync(properties);
+        }
+
+        private Task<bool> HandleSignout()
+        {
+            return Task.FromResult(false);
+        }
+
+        private async Task<bool> HandleSignIn()
+        {
+            if (Request.Path.Value.EndsWith(Options.AssertionConsumerServiceUrl, StringComparison.OrdinalIgnoreCase))
+            {
+                const string redirectUrl = "/Account/ExternalLoginCallback";
+                var properties = new AuthenticationProperties
+                {
+                    RedirectUri = redirectUrl,
+                    Items = { new KeyValuePair<string, string>("LoginProvider", Scheme.Name) }
+                };
+
+                var claims = new List<Claim>
+                {
+                    new Claim("subject", Guid.NewGuid().ToString()),
+                    new Claim("name", "J K M"),
+                    new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+                    new Claim(ClaimTypes.Email, $"{Guid.NewGuid().ToString()}@gmail.com")
+                };
+                var identity = new ClaimsIdentity(claims, Scheme.Name);
+
+                var principal = new ClaimsPrincipal(identity);
+
+                await Context.SignInAsync(Options.SignInScheme, principal, properties);
+
+                Response.Redirect(redirectUrl);
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
