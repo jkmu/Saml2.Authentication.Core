d680a7e30022003c567c43e8d7a377915d915ed3
Jessy Kyalo Musyimi <kyalo.jessy@gmail.com>
2019-02-17 20:40:42 +0100

    [patch] update to netcore 2.2, use https://stubidp.sustainsys.com for testing

--- Source/DemoWebApp/Startup.cs
+++ Source/DemoWebApp/Startup.cs
@@ -1,13 +1,15 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using DemoWebApp.Data;
+﻿using DemoWebApp.Data;
 using DemoWebApp.Models;
 using DemoWebApp.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Saml2.Authentication.Core.Options;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.AspNetCore.Http;
 
 namespace DemoWebApp
 {
@@ -34,16 +36,21 @@ namespace DemoWebApp
             services.AddTransient<IEmailSender, EmailSender>();
             services.AddTransient<IUserClaimsPrincipalFactory<ApplicationUser>, DemoWebAppClaimsPrincipalFactory>();
 
-            services.Configure<Saml2Configuration>(Configuration.GetSection("Saml2"));
+            services.Configure<Saml2Configuration>(Configuration.GetSection("Saml2:Sustainsys"));
 
             services.AddSaml();
-            services.AddSigningCertificates(
-                Configuration["Saml2:ServiceProviderConfiguration:SigningCertificateThumprint"],
-                Configuration["Saml2:IdentityProviderConfiguration:SigningCertificateThumprint"]);
+            services.AddSigningCertificatesFromFile(
+                "certificates\\demowebapp.local.pfx", "pass", X509KeyStorageFlags.PersistKeySet,
+                "certificates\\stubidp.sustainsys.com.cer");
 
             services.AddAuthentication()
-                .AddCookie()
-                .AddSaml(options => { options.SignInScheme = IdentityConstants.ExternalScheme; });
+                .AddCookie(options =>
+                {
+                    options.Cookie.HttpOnly = true;
+                    options.Cookie.SameSite = SameSiteMode.None;
+                    options.Cookie.SecurePolicy = CookieSecurePolicy.SameAsRequest;
+                })
+                .AddSaml(options => options.SignInScheme = IdentityConstants.ExternalScheme);
 
             services.AddMvc();
         }
@@ -51,11 +58,15 @@ namespace DemoWebApp
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                context.Database.EnsureCreated();
+            }
+
             if (env.IsDevelopment())
             {
-                app.UseBrowserLink();
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
             }
             else
             {
